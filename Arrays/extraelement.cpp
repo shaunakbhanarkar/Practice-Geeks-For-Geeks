2aa7c6f9ec4309df72abed49e18b45399350c3e3
Shaunak Bhanarkar <shaunak.bhanarkar@gmail.com>
2019-06-23 13:08:28 +0530

    Create extraelement.cpp

---
+++ Arrays/extraelement.cpp
@@ -0,0 +1,12 @@
+/*This is a function problem.You only need to complete the function given below*/
+/*Complete the function below*/
+int findExtra(int a[],int b[],int n)
+{
+ //add code here.
+ 
+    for (int i=0;i<n;i++)
+    {
+        if(a[i]!=b[i])
+            return i;
+    }
+}
