b96f6ea8bc3d17439070a04ed9799288c9372160
Shaunak Bhanarkar <shaunak.bhanarkar@gmail.com>
2019-06-23 14:17:01 +0530

    Create peakelement.cpp

---
+++ Arrays/peakelement.cpp
@@ -0,0 +1,21 @@
+/*This is a function problem.You only need to complete the function given below*/
+/* The function should return the index of any
+   peak element present in the array */
+// arr: input array
+// n: size of array
+int peakElement(int arr[], int n)
+{
+   // Your code here
+   if (arr[0]>=arr[1])
+        return 0;
+    if (arr[n-1]>=arr[n-2])
+        return n-1;
+    
+    for (int i=1;i<=n-2;i++)
+    {
+        if (arr[i]>=arr[i-1] && arr[i]>=arr[i+1])
+            return i;
+    }
+    
+    return -1;
+}
