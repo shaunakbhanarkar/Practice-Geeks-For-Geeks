a8a7ca7b16d03196fd596781d37e66131c99d380
Shaunak Bhanarkar <shaunak.bhanarkar@gmail.com>
2019-06-29 14:22:04 +0530

    Create kdistancefromroot.cpp

---
+++ Trees/kdistancefromroot.cpp
@@ -0,0 +1,43 @@
+/*This is a function problem.You only need to complete the function given below*/
+/* A binary tree node has data, pointer to left child
+   and a pointer to right child /
+struct Node
+{
+    int data;
+    Node* left;
+    Node* right;
+}; */
+// function should print the nodes at k distance from root
+void printKdistance(Node *root, int k)
+{
+  // Your code here
+  if (root == NULL)
+    return;
+  if (k==0)
+    cout<<root->data<<" ";
+    
+    if (root->left != NULL && root->right != NULL)
+    {
+           printKdistance(root->left,k-1);
+           printKdistance(root->right,k-1);
+           return;
+    }
+    
+    if (root->left != NULL && root->right == NULL)
+    {
+           printKdistance(root->left,k-1);
+           return;
+    }
+    
+    if (root->left == NULL && root->right != NULL)
+    {
+           printKdistance(root->right,k-1);
+           return;
+    }
+    
+    if (root->left == NULL && root->right == NULL)
+    {
+           
+           return;
+    }
+}
