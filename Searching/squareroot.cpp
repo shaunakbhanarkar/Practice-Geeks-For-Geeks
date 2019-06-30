a1ba9b0ff5b15116978a83ac7bcb294e59fa11ae
Shaunak Bhanarkar <shaunak.bhanarkar@gmail.com>
2019-06-30 20:24:57 +0530

    Create squareroot.cpp

---
+++ Searching/squareroot.cpp
@@ -0,0 +1,9 @@
+/*This is a function problem.You only need to complete the function given below*/
+
+// Function to find square root
+// x: element to find square root
+long long int floorSqrt(long long int x) 
+{
+    // Your code goes here   
+    return sqrt(x);
+}
