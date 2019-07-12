239954715df3945a90d1699e77ff59be7238d07d
Shaunak Bhanarkar <shaunak.bhanarkar@gmail.com>
2019-07-12 20:10:54 +0530

    Create sumofdependencies.cpp

---
+++ Graphs/sumofdependencies.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+int main()
+ {
+	//code
+	
+	int t;
+	cin>>t;
+	
+	while(t--)
+	{
+	    int n,e;
+
+        cin>>n>>e;
+        
+        // int a[n][n];
+        
+        for (int i=0;i<e;i++)
+        {
+            int u,v;
+            cin>>u;
+            cin>>v;
+            
+        }
+        
+        cout<<e<<endl;
+	}
+	return 0;
+}
