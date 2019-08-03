c1158be10db5802057c953b87ce2d6105bf52fbc
Shaunak Bhanarkar <shaunak.bhanarkar@gmail.com>
2019-08-03 16:16:04 +0530

    Create reverse-string.cpp

---
+++ Strings/reverse-string.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+ {
+	//code
+	int t;
+	cin>>t;
+	
+	while(t--)
+	{
+	    string s;
+	    cin>>s;
+	    
+	    for (int i=s.size()-1;i>=0;i--)
+	    {
+	        cout<<s[i];
+	    }
+	    cout<<endl;
+	}
+	return 0;
+}
