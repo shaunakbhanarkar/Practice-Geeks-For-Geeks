52c92182bbcbfab9944e040983cf5d1cac7fa9a2
Shaunak Bhanarkar <shaunak.bhanarkar@gmail.com>
2019-08-09 18:56:11 +0530

    Create postfix-to-infix.cpp

---
+++ Stack/postfix-to-infix.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool isOperator(char c)
+{
+    if (c == '+' || c == '-' || c == '*' || c == '/')
+        return true;
+    return false;
+}
+
+string postfix_to_infix (string postfix)
+{
+    stack<string> s;
+    
+    for (int i=0; i<postfix.size(); i++)
+    {
+        if (isOperator(postfix[i]))
+        {
+            string op1 = s.top();
+            s.pop();
+            
+            string op2 = s.top();
+            s.pop();
+            
+            string temp = "(" + op1 + postfix[i] + op2 + ")";
+            
+            s.push(temp);
+        }
+        else
+        {
+            string temp;
+            temp.push_back(postfix[i]);
+            
+            s.push(temp);
+        }
+    }
+    return s.top();
+}
+
+int main() {
+    int t;
+    cin>>t;
+    
+    while(t--)
+    {
+        string postfix;
+        cin>>postfix;
+        
+        cout << postfix_to_infix( postfix) <<endl;
+    }
+	return 0;
+}
